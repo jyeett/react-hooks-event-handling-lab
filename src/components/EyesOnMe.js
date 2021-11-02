2e0f1300c9fd161394d35f914e7e4c934e4d88ab
Jordan Yee <jordanyee799@gmail.com>
2021-11-02 14:21:52 -0700

    completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+    return (
+        <button onFocus={focus} onBlur={blur}>Eyes on me</button>
+    )
+}
+
+function focus() {
+    console.log('Good!')
+}
+
+function blur () {
+    console.log('Hey! Eyes on me!')
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+function Keypad() {
+    return (
+        <>
+            <input type='password' onChange={enterPassword}></input>
+        </>
+    )
+}
+
+function enterPassword() {
+    console.log('Entering password...')
+}
+
+export default Keypad
